d7bbc638d84d5346c2ab57fc9f81167678392ccb
João Victor <hauntermonster68@gmail.com>
2023-07-08 16:14:08 -0300

    Resolução do exercício três.

---
+++ src/ExercicioTres.java
@@ -0,0 +1,34 @@
+class ExercicioTres {
+    public static String maiorPalindromo(String s) {
+        int comeco = 0;
+        int fim = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int imparChar = expand(s, i, i);
+            int parChar = expand(s, i, i + 1);
+
+            int tamanhoMaiorPalindromo = Math.max(imparChar, parChar);
+
+            if (tamanhoMaiorPalindromo > fim - comeco) {
+                comeco = i - (tamanhoMaiorPalindromo - 1) / 2;
+                fim = i + tamanhoMaiorPalindromo / 2;
+
+            }
+        }
+        return s.substring(comeco, fim + 1);
+    }
+
+    static int expand(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
+
+    public static void main(String[] args) {
+        String s = "ababad";
+        System.out.println(maiorPalindromo(s));
+
+    }
+}
